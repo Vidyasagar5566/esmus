58349a4253d8d7598ee5b60f53e496f3d305cb03
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2023-12-11 00:37:44 +0530

    start

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello Guys! This is Esmus Technologies</h1>
-     <h1>Thank You for Visitiong Our Websitie</h1>
+      <header>
+        <h1>esmus.in</h1>
+        <p>Where Connection Meets Convenience for IIT and NIT Students</p>
+      </header>
+
+      <section>
+          <h2>Download Now and Embark on a Journey of Enhanced Collaboration, Communication, and Community!</h2>
+          <p>
+              Welcome to esmus.in, a platform designed to bring IIT and NIT students together. Experience a new level of collaboration, communication, and community-building tailored to your needs.
+          </p>
+          <p>
+              Download now and unlock the potential for seamless connections, convenient interactions, and a thriving community of like-minded individuals.
+          </p>
+          <a href="#" class="cta-button">Download Now</a>
+      </section>
     </div>
   );
 }
